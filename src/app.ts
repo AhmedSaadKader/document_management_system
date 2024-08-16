1fc7b57b446ff2814f275c75d868b55327395ae8
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-08-17 00:24:20 +0300

    app.ts: Initialize the express app

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || '5000';
+const address = `localhost:${port}`;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', function (req: Request, res: Response) {
+  res.send('Documnet Management System');
+});
+
+app.listen(port, () => {
+  console.log(`Starting app on: ${address}`);
+});
+
+export default app;
