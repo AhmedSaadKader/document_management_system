0dd003d56a3a8f6ad09a80951d06807ad73ec985
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-09-16 12:12:15 +0300

    fixup! fixup! cors, lf and env production

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ if (process.env.ENV == 'production') {
   console.log('production');
   app.use(
     cors({
-      origin: ['http://localhost:3000', 'https://dms-app.netlify.app/'],
+      origin: ['https://dms-app.netlify.app'],
       credentials: true,
       exposedHeaders: ['Content-Disposition'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
